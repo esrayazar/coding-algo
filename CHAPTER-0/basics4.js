24b8bea56f3265a7706fd9953f04f80bf7557b5e
Esra Yazar <esrayzr@gmail.com>
2022-02-07 15:32:26 -0500

    basic4

--- CHAPTER-0/basics4.js
+++ CHAPTER-0/basics4.js
@@ -2,3 +2,9 @@
  * Array with Odds
  * Create an array with all the odd integers between 1 and 255 (inclusive).
  */
+
+var arr = [];
+for(var num=1; num<=255; num+=2){
+    arr.push(num);
+}
+console.log(arr);
